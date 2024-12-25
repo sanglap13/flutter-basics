4934332cda44fcfa82477772ee5711282b499f39
sanglap13 <sanglapmridha@gmail.com>
2024-12-25 22:52:56 +0530

    feat: add basic home page and understood stateless widget

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/views/home_view.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Learning",
+      home: HomeView()
+    );
+  }
+}
---
+++ lib/views/home_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Container(
+          color: Colors.deepOrange,
+          child: Center(
+            child: const Text(
+              "Yo Yo",
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.white,
+                 fontWeight: FontWeight.bold
+              ),
+              ),
+          ),
+        ),
+      );
+  }
+}
