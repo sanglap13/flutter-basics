f149b32c93712895eda2069a8b7e82e3ced789c0
sanglap13 <sanglapmridha@gmail.com>
2025-01-05 23:51:47 +0530

    chore: add test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,9 +3,9 @@ import 'package:flutter_application_1/app/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets("Yo Yo Test", (WidgetTester tester) async {
+  testWidgets("Calc Test", (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    expect(find.byType(Container), findsOneWidget);
+    expect(find.byWidget(const Text("0")), findsOneWidget);
   });
 }
